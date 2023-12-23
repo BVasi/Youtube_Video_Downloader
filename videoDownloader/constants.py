6faef86d72197aa23e0956c34e486833ccf238a0
BVasi <vasilica_badescu@yahoo.com>
2023-12-23 21:17:50 +0200

    first commit

---
+++ videoDownloader/constants.py
@@ -0,0 +1,13 @@
+GET = 'GET'
+POST = 'POST'
+
+DEFAULT_ROUTE = '/'
+SEARCH_ROUTE = '/search'
+DOWNLOAD_ROUTE = '/download'
+
+SEARCH_TEMPLATE = 'search.html'
+RESULTS_TEMPLATE = 'results.html'
+
+MP4 = 'mp4'
+
+QUERY_LIMIT = 5
---
+++ videoDownloader/video.py
@@ -0,0 +1,5 @@
+class Video():
+    def __init__(self, title, length, link) -> None:
+        self.title = str(title)
+        self.length = str(length)
+        self.link = str(link)
